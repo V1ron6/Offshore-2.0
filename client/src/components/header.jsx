273b059805572e022467ced6f339902dd90006ee
bytebandit,viron <mindedbusines@gmail.com>
2025-12-07 19:14:29 +0000

    backup point

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,17 +3,16 @@
 const header=()=>{
 	return(
 		<>
-		<header class="bg-red-300 h-14  flex justify-space-between  w-full border-b-2 border-grey-200 rounded-b-md ">
+		<header className="bg-red-300 h-14  flex justify-space-between  w-full border-b-2 border-grey-200 rounded-b-md ">
 			<div>
-					<h1 class="justify-end text-lg  p-2">Offshore</h1>
+					<h1 className="justify-end text-lg  p-2">Offshore</h1>
 			</div>
-		  <div class="h-7 w-16  border-1 rounded-md m-2">
-				<button class="pl-4 text-black  dark:text-white hover:text-white ">login</button>
+		  <div className="h-7 w-16  border-1 rounded-md m-2">
+				<button className="pl-4 text-black  dark:text-white hover:text-white ">login</button>
 				</div> 
 		</header>
 		</>
 
 	)
 }
-
 export default  header;
