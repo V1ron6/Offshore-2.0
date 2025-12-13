2d91c22a62ddf2896ef007eed99b431fc721db17
bytebandit,viron <mindedbusines@gmail.com>
2025-12-13 22:11:58 +0000

    almos done with login design

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,20 +10,24 @@ import Signup from "./pages/signup.jsx"
 import Notfound from "./pages/notfound.jsx"
 import Home from "./pages/home.jsx"
 import App from "./App.jsx"
+import Profile from "./pages/profile.jsx"
+import Dashboard from "./pages/dashboard.jsx"
 
 
 
-const router = createBrowserRouter([
+const flipper = createBrowserRouter([
  {path:"/" ,element: <App />},
  {path:"/signup",element: <Signup />},
- {path:"/home",element: <Home />},
+ {path:"/home/id",element: <Home />},
  {path:"/login", element:<Login />},
+ {path:"/profile/:id/p",element: <Profile />},
+ {path:"/dashboard/:id",element:<Dashboard />},
  {path:"*",element:<Notfound />}
 
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router} />
+      <RouterProvider router={flipper} />
   </StrictMode>,
 )
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,34 +1,39 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import Offshorelogo from '../assets/Offshorelogo.jpg'
+const button = " m-1 w-30 border border-black p-1 logoColor rounded-md"
+const buttonCenter =button + " w-60"
+
+
 const Login=()=> {
 	return (
 		<>
-		<div className="flex min-h-screen items-center justify-center border-1">
+		<div className="flex min-h-screen items-center justify-center border prefFont">
 			<div>
-			<div className='relative bottom-10 border-1 z-1'>
-				<h1 className='font-bold text-2xl relative bottom-8 text-center z-{-1}'>Off<span className='text-red-300 '>Shore</span></h1>
+			<div className='relative bottom-10  z-1'>
+				<h1 className='font-bold text-5xl relative bottom-8 text-center z-{-1}'>Off<span className='text-red-300 '>Shore</span></h1>
 			</div>
 				
-			<form action="" className=" z-{-1} border-2 p-4 h-80 rounded-md relative botom-16" >
-				<div className="z-1 relative top-20">
-				<div className="border-1 rounded-md">
+			<form action="" className=" z-{-1} border-2 p-4 h-100  rounded-md relative botom-16" >
+				<div className="z-1 relative top-10 ">
+				<div className="border rounded-md flex justify-evenly">
 					<Link to={"/signup"}>
-					<button className=" m-1 w-30 border-1 p-1 rounded-md">Signup</button>
+					<button className=" m-1 w-30 border border-black p-1 logoColor rounded-md">Signup</button>
 					</Link>
 						<Link to={"/login"}>
-					<button className="m-1 w-30 border-1 p-1 rounded-md" >Login</button>
+					<button className={button} >Login</button>
 					</Link>
 				</div>
-				<div className="border-1 relative bottom-0 m-1">
-					<label>Username:</label>
+				<div className="border relative bottom-0 m-1 p-4  justify-center ">
+					<label >Username:</label>
 					<br />
-					<input type="text" name="username" className="border-1 " />
+					<input type="text" name="username" className="border rounded-lg m-2 p-1" />
 					<br />
-					<label >password</label>
+					<label >Password</label>
 					<br />
-					<input type="password" name="password" className="border-1"/>
+					<input type="password" name="password" className="border rounded-lg m-2 p-1"/>
 					<br />
+					<input type="submit" name="login" value="login" className={buttonCenter} />
 				</div>
 			</div>
 			</form>
